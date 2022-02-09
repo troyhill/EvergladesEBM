6363ab42f4a1f12800146d0cf3c5a8ae78401fa4
troyhill <hill.troy@gmail.com>
2022-02-09 18:55:28 -0500

    updated plotPM

--- R/plotPM.R
+++ R/plotPM.R
@@ -13,7 +13,7 @@
 #' @importFrom ggplot2 theme_classic
 #' @importFrom ggplot2 geom_line
 #' @importFrom ggplot2 scale_colour_brewer
-#' @importFrom ggplot2 ylim
+#' @importFrom ggplot2 coord_cartesian
 #' @importFrom ggplot2 ylab
 #' @importFrom ggplot2 xlab
 #' @importFrom ggplot2 labs
@@ -30,13 +30,13 @@ plotPM <- function(quantileData,# = quantDat_dd, # value must be in variable nam
   
   ### creates quantile plot with overlaid EDEN trace
   ggplot2::ggplot(quantileData, ggplot2::aes_string(x = "date", y = "ave")) + 
-    ggplot2::theme_classic() + 
+    ggplot2::theme_bw() + 
     ggplot2::geom_line(ggplot2::aes_string(color = "quantile"), size = 1.5) + #facet_wrap(. ~ name) + 
     ggplot2::scale_colour_brewer(palette = "RdYlBu") + 
     ggplot2::ylab("Stage (ft. NGVD29)") + 
     ggplot2::xlab("")  + 
     ggplot2::labs(color='Quantile') + 
     ggplot2::geom_line(data = EDENdata[(EDENdata$date >= (min(quantileData$date) - daysToPlot)) & (EDENdata$date < max(quantileData$date)),], 
-              mapping = ggplot2::aes_string(x = "date", y = "ave"), size = 1.5)+ 
-    ggplot2::ylim(min(c(quantileData$ave, EDENdata$ave), na.rm = TRUE), max(c(quantileData$ave, EDENdata$ave), na.rm = TRUE))
+              mapping = ggplot2::aes_string(x = "date", y = "ave"), size = 1.5)#+ 
+    # ggplot2::coord_cartesian(min(c(quantileData$ave, EDENdata$ave), na.rm = TRUE), max(c(quantileData$ave, EDENdata$ave), na.rm = TRUE))
 }
